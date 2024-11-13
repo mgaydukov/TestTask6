d22ecc610469b27fe45c9d291f5c7dce0e683680
Maxim G <gma_w@mail.ru>
2024-11-13 04:28:46 +0300

    Init t3 branch

---
+++ src/main/java/com/moysklad/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.moysklad.repository;
+
+import com.moysklad.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
